b9cd7c22fa9f08c42ba4179622b09fcf7dc1b609
kamal kishore <kamal.kishore981@gmail.com>
2022-08-30 16:22:12 -0400

    built  the database cell

--- server.js
+++ server.js
@@ -24,9 +24,43 @@ console.log('Connected to the election database.')
 
 // Default response for any other request (Not Found)
 // this query method allow to write sql command in node.js application
-db.query(`SELECT * FROM candidates`,(err,rows)=>{
-    console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`,(err,rows)=>{
+//     console.log(rows);
+// });
+
+// Get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id =1`,(err,row)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`,1,(err,result)=>{
+//     if(err){
+//      console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// create a candidate
+// const sql = `INSERT INTO candidates(id,first_name,last_name,industry_connected)
+//     VALUES (?,?,?,?)`;
+// const params = [1,'Ronland','Firbank',1];
+
+// db.query(sql,params,(err,result)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+
+
+
+
+
 app.use((req, res) => {
   res.status(404).end();
 });
