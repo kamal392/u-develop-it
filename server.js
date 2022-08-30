039ee4413c1b88ef3cd3d41e70c26a0f8483a3ff
kamal kishore <kamal.kishore981@gmail.com>
2022-08-30 15:41:18 -0400

    connected Mysql (database) to node.js application

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+
+const mysql = require('mysql2');
 const express = require('express');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,8 +10,23 @@ const app = express();
 app.use(express.urlencoded({ extended:false}));
 app.use(express.json());
 
-// Default response for any other request (Not Found)
+// connect to database
+const db = mysql.createConnection({
+    host : 'localhost',
+    // your Mysql username,
+    user:'root',
+    // your Mysql password
+    password:'Kishore@123',
+    database :'election'
+},
+console.log('Connected to the election database.')
+);
 
+// Default response for any other request (Not Found)
+// this query method allow to write sql command in node.js application
+db.query(`SELECT * FROM candidates`,(err,rows)=>{
+    console.log(rows);
+});
 app.use((req, res) => {
   res.status(404).end();
 });
