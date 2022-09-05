b98336f72a0714b62e018bcb4c0a73b1abb8ced7
kamal kishore <kamal.kishore981@gmail.com>
2022-09-05 02:45:04 -0400

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,5 +3,6 @@ const express = require("express");
 const router = express.Router();
 router.use(require("./partyRoutes"));
 router.use(require("./candidateRoutes"));
+router.use(require('./voterRoutes'));
 
 module.exports = router;
