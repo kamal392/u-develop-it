080ac5611c45598988ed8465bd0734c2afbe54f0
kamal kishore <kamal.kishore981@gmail.com>
2022-09-06 23:04:25 -0400

    votes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 
 const express = require("express");
+const { route } = require("./voteRoutes");
 const router = express.Router();
 router.use(require("./partyRoutes"));
 router.use(require("./candidateRoutes"));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
 
 module.exports = router;
